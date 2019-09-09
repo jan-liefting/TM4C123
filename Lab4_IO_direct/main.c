6a300a548729b02ac1af95719f091c38c7236a32
jan-liefting <jan-liefting@users.noreply.github.com>
2019-09-09 20:15:21 +0200

    2

--- Lab4_IO_direct/main.c
+++ Lab4_IO_direct/main.c
@@ -19,20 +19,7 @@
 // blue LED connected to PF2 on the Launchpad
 // green LED connected to PF3 on the Launchpad
 
-// 1. Pre-processor Directives Section
-// Constant declarations to access port registers using
-// symbolic names instead of addresses
-// TExaS.o is the object code for the real-board grader
-//#define GPIO_PORTF_DATA_R       (*((volatile unsigned long *)0x400253FC))
-//#define GPIO_PORTF_DIR_R        (*((volatile unsigned long *)0x40025400))
-//#define GPIO_PORTF_AFSEL_R      (*((volatile unsigned long *)0x40025420))
-//#define GPIO_PORTF_PUR_R        (*((volatile unsigned long *)0x40025510))
-//#define GPIO_PORTF_DEN_R        (*((volatile unsigned long *)0x4002551C))
-//#define GPIO_PORTF_LOCK_R       (*((volatile unsigned long *)0x40025520))
-//#define GPIO_PORTF_CR_R         (*((volatile unsigned long *)0x40025524))
-//#define GPIO_PORTF_AMSEL_R      (*((volatile unsigned long *)0x40025528))
-//#define GPIO_PORTF_PCTL_R       (*((volatile unsigned long *)0x4002552C))
-//#define SYSCTL_RCGC2_R          (*((volatile unsigned long *)0x400FE108))
+// 0. Pre-processor Directives Section
 
 // 1. Includes
 #include <stdint.h>
