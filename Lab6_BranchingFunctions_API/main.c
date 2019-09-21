e30221283885fb342769fc509b0ab22f685d918b
jan-liefting <jan-liefting@users.noreply.github.com>
2019-09-21 21:01:28 +0200

    final

--- Lab6_BranchingFunctions_API/main.c
+++ Lab6_BranchingFunctions_API/main.c
@@ -21,33 +21,34 @@
 #include "sysctl.h"
 #include "hw_memmap.h"
 
-// hier wordt een unsigned long HEX getal eerst uitgerekend en daarna omgezet naar een pointer!
-#define SW1                     (*((volatile unsigned long *)(GPIO_PORTF_BASE + (0x10 << 2)))) //SW1 on PF4
-#define Blue_LED                (*((volatile unsigned long *)(GPIO_PORTF_BASE + (0x04 << 2)))) //blue LED on PF2
-
 // function prototype
 void Init_PF(void);
 void Delay100ms(unsigned long);
 
 int main(void)
 {
+    int32_t x;
+
     // initialization goes here
+    // set clock PLL 80 MHz
     SysCtlClockSet(SYSCTL_SYSDIV_2_5 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ); // set system clock to 80 MHz
     volatile uint32_t clock = SysCtlClockGet(); // get system clock
 
     Init_PF();
 
-    Blue_LED = 0x04;                          // turn blue LED on PF2
+    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, GPIO_PIN_2);
+
     while (1)
     {
         // body goes here
-        if (!(SW1 & 0x10))
+        if (!(GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_4)))
         {                       // if SW is pressed (negative logic)
-            Blue_LED ^= 0x04;                   // toggle Blue_LED
-            Delay100ms(4);                      // delay toggle
+            x = GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_2) ^ GPIO_PIN_2;  // Toggle pin 2
+            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, x);
+            Delay100ms(4);      // delay toggle
         }
         else
-            Blue_LED = 0x04;                    // turn blue LED on PF2
+            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, GPIO_PIN_2); // turn blue LED on PF2
     }
 }
 
@@ -74,22 +75,13 @@ void Delay100ms(unsigned long time)
 // Assumes:
 void Init_PF()
 {
-//    volatile unsigned long delay;
-//    SYSCTL_RCGC2_R |= 0x00000020;   // 1) F clock
-//    delay = SYSCTL_RCGC2_R;         // delay to allow clock to stabilize
-//    GPIO_PORTF_AMSEL_R &= 0x00;     // 2) disable analog function
-//    GPIO_PORTF_PCTL_R &= ~0x000F0F00;   // 3) GPIO clear bit PCTL PF4 and PF2
-//    GPIO_PORTF_DIR_R &= ~0x10;      // 4.1) PF4 input,
-//    GPIO_PORTF_DIR_R |= 0x04;       // 4.2) PF2 output
-//    GPIO_PORTF_AFSEL_R &= 0x00;     // 5) no alternate function
-//    GPIO_PORTF_PUR_R |= 0x10;       // 6) enable pullup resistor on PF4 (negative logic)
-//    GPIO_PORTF_DEN_R |= 0x14;       // 7) enable digital pins PF4 and PF2
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);  // 1) F enable
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);        // 1) F enable
     while (!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF))
-    {                                          // 2) wait for port F to be ready
+    {                                                   // 2) wait for port F to be ready
     }
-    GPIOPinTypeGPIOInput(GPIO_PORTF_BASE, GPIO_PIN_4); // PF4 input
+    GPIOPinTypeGPIOInput(GPIO_PORTF_BASE, GPIO_PIN_4);  // PF4 input
     GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_2); // PF2 output
     // enable pullup resistors on PF4
     GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_4, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
 }
+
