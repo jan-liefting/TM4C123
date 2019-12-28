545365cd30722cc22d3c301105f5d58f9410f640
jan <jan@jan-Aspire-XC-705>
2019-12-28 20:03:42 +0100

    tjonge

--- Lab4_IO_API/main.c
+++ Lab4_IO_API/main.c
@@ -49,41 +49,21 @@ int main(void)
 
         switch (SW)
         {
-        case (0x00):
+        case (0x00): // neither are pressed; LED is off
                     GPIO_PORTF_DATA_R = 0x00;
-        break; // neither are pressed; LED is off
-        case (0x01):
+        break;
+        case (0x01): // SW2 pressed; LED is green
                 GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3 | GPIO_PIN_2 |GPIO_PIN_1, GPIO_PIN_3);
-        break; // SW2 pressed; LED is green
-        case (0x10):
+        break;
+        case (0x10): // SW1 pressed; LED is red
                 GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3 | GPIO_PIN_2 |GPIO_PIN_1, GPIO_PIN_1);
-        break; // SW1 pressed; LED is red
-        case (0x11):
+        break;
+        case (0x11): // both pressed; LED is blue
                 GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3 | GPIO_PIN_2 |GPIO_PIN_1, GPIO_PIN_2);
-        break; // both pressed; LED is blue
+        break;
         }
     }
 }
-// Subroutine to initialize port F pins for input and output
-// PF4 and PF0 are input SW1 and SW2 respectively
-// PF3,PF2,PF1 are outputs to the LED
-// Inputs: None
-// Outputs: None
-// Notes: These five pins are connected to hardware on the LaunchPad
-//void PortF_Init(void)
-//{
-//    volatile unsigned long delay;
-//    SYSCTL_RCGC2_R |= 0x00000020;     // 1) F clock
-//    delay = SYSCTL_RCGC2_R;           // delay
-//    GPIO_PORTF_LOCK_R = 0x4C4F434B;   // 2) unlock PortF PF0
-//    GPIO_PORTF_CR_R = 0x1F;           // allow changes to PF4-0
-//    GPIO_PORTF_AMSEL_R = 0x00;        // 3) disable analog function
-//    GPIO_PORTF_PCTL_R = 0x00000000;   // 4) GPIO clear bit PCTL
-//    GPIO_PORTF_DIR_R = 0x0E;          // 5) PF4,PF0 input, PF3,PF2,PF1 output
-//    GPIO_PORTF_AFSEL_R = 0x00;        // 6) no alternate function
-//    GPIO_PORTF_PUR_R = 0x11;        // enable pullup resistors on PF4,PF0
-//    GPIO_PORTF_DEN_R = 0x1F;          // 7) enable digital pins PF4-PF0
-//}
 
 // Subroutine to initialize port F pins for input and output
 // PF4 and PF0 are input SW1 and SW2 respectively
@@ -95,15 +75,18 @@ void PortF_Init(void)
 {
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);  // 1) F enable
     while (!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF))
-    {                                            // 2) wait for port F to be ready
+    {                                          // 2) wait for port F to be ready
     }
     GPIO_PORTF_LOCK_R = GPIO_LOCK_KEY;  // 2) unlock PortF PF0
     GPIO_PORTF_CR_R = 0x1F;           // allow changes to PF4-0
     GPIOPinTypeGPIOInput(GPIO_PORTF_BASE, GPIO_PIN_4 | GPIO_PIN_0); // PF0, PF4 input
-    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_3 | GPIO_PIN_2 | GPIO_PIN_1); // PF3, PF2, PF1 output
+    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE,
+                          GPIO_PIN_3 | GPIO_PIN_2 | GPIO_PIN_1); // PF3, PF2, PF1 output
     // enable pullup resistors on PF4,PF0
-    GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_4 | GPIO_PIN_0, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
+    GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_4 | GPIO_PIN_0,
+                     GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
 }
+
 
 // Color    LED(s) PortF
 // dark     ---    0
